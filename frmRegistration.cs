4a006bf25f724a003e8cb72b622654b118d9a146
Luigi Marin Cabrera <116327738+LegendaryLog1c@users.noreply.github.com>
2022-10-21 17:34:51 +0800

    Add files via upload

---
+++ frmRegistration.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_LAB_EXERCISE___CABRERA
+{
+    public partial class frmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+        {
+
+            string[] ListOfProgram = new string[]
+            {
+
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS Tourism Management"
+
+            };
+
+            for(int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+
+        }
+
+        /////return methods 
+        public long StudentNumber(string studNum)
+        {
+
+            if (Regex.IsMatch(studNum, @"^[0-9]{10,11}$"))
+            {
+                _StudentNo = long.Parse(studNum);
+            }
+
+            else
+            {
+                throw new FormatException("Invalid student number.");
+            }
+               
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+
+            else
+            {
+                throw new FormatException("Invalid contact number.");
+            }
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") && Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") && Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+
+            else
+            {
+                throw new FormatException("Invalid name.");
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+
+            else
+            {
+                throw new FormatException("Invalid age.");
+            }
+
+            return _Age;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StudentInformationClass.SetFullName = FullName(txtLastName.Text,
+                txtFirstName.Text, txtMiddleInitial.Text);
+
+                StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+                StudentInformationClass.SetProgram = cbPrograms.Text;
+                StudentInformationClass.SetGender = cbGender.Text;
+                StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+                StudentInformationClass.SetAge = Age(txtAge.Text);
+                StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+
+                frmConfirmation frm = new frmConfirmation();
+                frm.ShowDialog();
+
+            }
+
+            catch(FormatException ab)
+            {
+                MessageBox.Show(ab.Message);
+            }
+
+
+
+        }
+
+        private void cbGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
